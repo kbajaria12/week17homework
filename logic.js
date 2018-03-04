95fa30d69be4ea937dff5073dd0f53ba5f2da81a
kbajaria <kbajaria@gmail.com>
2018-03-03 19:50:47 -0800

    Got legend working

--- logic.js
+++ logic.js
@@ -20,7 +20,28 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 function markerSize(magnitude){
-    return magnitude * 10;
+    return magnitude * 5;
+}
+
+function markerColor(magnitude){
+    if (magnitude <1) {
+        return("LightGreen");
+    }
+    else if (magnitude <2){
+        return ("GreenYellow");
+    } 
+    else if (magnitude <3){
+        return ("Yellow");
+    }
+    else if (magnitude <4){
+        return ("Pink");
+    }
+    else if (magnitude <5) {
+        return ("Orange");
+    }
+    else {
+        return ("Red");
+    }
 }
 
 // Grabbing our GeoJSON data..
@@ -37,7 +58,7 @@ d3.json(link, function(data) {
                     return {fillOpacity: .5,
                             color:0,
                             weight:0,
-		    		        fillColor: "purple",
+		    		        fillColor: markerColor(feature.properties.mag),
 		    		        radius: markerSize(feature.properties.mag)};
                 },
                 // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
@@ -53,5 +74,28 @@ var cone = L.marker([33.640495, -117.844296], {
  	draggable: true,
  	title: "UCI"
 }).addTo(map);
+
  //Binding a pop-up to our marker
 cone.bindPopup("Data Analytics");
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+    
+    div.innerHTML += '<b>Magnitude</b><br>';
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + markerColor(grades[i]) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
